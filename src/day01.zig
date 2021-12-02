7c37ca368957815e564704162c3e08328d9573b2
Louis Pearson <louispearson@librem.one>
2021-12-01 19:18:33 -0700

    Use full path for std library functions

--- src/day01.zig
+++ src/day01.zig
@@ -12,7 +12,7 @@ const gpa = util.gpa;
 const data = @embedFile("../data/day01.txt");
 
 pub fn main() !void {
-    var tokenIterator = tokenize(u8, data, " \n");
+    var tokenIterator = std.mem.tokenize(u8, data, " \n");
     var window: [3]i32 = .{ 0, 0, 0 };
     var previousSum: ?i32 = null;
     var increaseCount: usize = 0;
@@ -27,44 +27,16 @@ pub fn main() !void {
         // Only sum index if we've seen at least 3 values
         if (windowIndex >= 2) {
             var sum = window[0] + window[1] + window[2];
-            print("{} + {} + {} = {}", .{ window[0], window[1], window[2], sum });
+            std.debug.print("{} + {} + {} = {}", .{ window[0], window[1], window[2], sum });
             if (previousSum) |lastSum| {
                 if (sum > lastSum) {
                     increaseCount += 1;
-                    print(" (increase)", .{});
+                    std.debug.print(" (increase)", .{});
                 }
             }
             previousSum = sum;
-            print("\n", .{});
+            std.debug.print("\n", .{});
         }
     }
-    print("Increases: {}, lastSum: {}, windowIndex: {}\n", .{ increaseCount, previousSum, windowIndex });
+    std.debug.print("Increases: {}, lastSum: {}, windowIndex: {}\n", .{ increaseCount, previousSum, windowIndex });
 }
-
-// Useful stdlib functions
-const tokenize = std.mem.tokenize;
-const split = std.mem.split;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
-
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
-
-const min = std.math.min;
-const min3 = std.math.min3;
-const max = std.math.max;
-const max3 = std.math.max3;
-
-const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.sort;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
