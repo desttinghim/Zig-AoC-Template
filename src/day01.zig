3cc949932e71d6a90cd1a320d2a9116c6d6ec28b
Louis Pearson <louispearson@librem.one>
2021-12-01 19:20:08 -0700

    Removed unused functions

--- src/day01.zig
+++ src/day01.zig
@@ -1,13 +1,4 @@
 const std = @import("std");
-const Allocator = std.mem.Allocator;
-const List = std.ArrayList;
-const Map = std.AutoHashMap;
-const StrMap = std.StringHashMap;
-const BitSet = std.DynamicBitSet;
-const Str = []const u8;
-
-const util = @import("util.zig");
-const gpa = util.gpa;
 
 const data = @embedFile("../data/day01.txt");
 
