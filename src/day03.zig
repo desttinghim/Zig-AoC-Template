2e6fc23aac3c88d43fd987f0e9ea35c6c48e8c74
Louis Pearson <louispearson@librem.one>
2021-12-03 09:09:36 -0700

    stuff

--- src/day03.zig
+++ src/day03.zig
@@ -11,8 +11,34 @@ const gpa = util.gpa;
 
 const data = @embedFile("../data/day03.txt");
 
+const bitCount = 12;
 pub fn main() !void {
-    
+    var binaryNumbers = ArrayList([bitCount]u1).init();
+    var total1Bits = try part1();
+    try part2(total1Bits);
+}
+
+fn part1() ![bitCount]u16 {
+    var tokenIter = tokenize(u8, data, " \r\n ");
+    var total: u16 = 0;
+    var total1Bits = [_]u16{0} ** bitCount;
+    while (tokenIter.next()) |token| : (total += 1) {
+        for (token) |bit, i| {
+            if (bit == '1') total1Bits[i] += 1;
+        }
+    }
+    var gamma: u32 = 0;
+    var epsilon: u32 = 0;
+    for (total1Bits) |bits, i| {
+        if (bits >= total / 2) gamma |= @as(u32, 1) << @intCast(u5, bitCount - i - 1);
+        if (bits <= total / 2) epsilon |= @as(u32, 1) << @intCast(u5, bitCount - i - 1);
+    }
+    print("gamma: {}, epsilon: {}, power: {}, total: {}, bitAvg: {any}\n", .{ gamma, epsilon, gamma * epsilon, total, total1Bits });
+    return total1Bits;
+}
+
+fn part2(total1Bits: [bitCount]u16) !void {
+    print("total1Bits: {any}", .{total1Bits});
 }
 
 // Useful stdlib functions
